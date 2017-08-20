24f23dd2dad3675ada251efd6fa33fcdb978b253
Rajesh Shedolkar <rajeshshedolkar12@gmail.com>
2017-08-20 18:34:41 +0530

    proper indentation

--- hellow.cpp
+++ hellow.cpp
@@ -1,55 +1,43 @@
 #include<iostream>
 using namespace std;
-void spiral(int A[3][6],int n ,int m)
-{
+void spiral(int A[3][6],int n ,int m){
+    int row_start=0;
+    int row_end=m;
+    int column_start=0;
+    int column_end=n;
 
-int row_start=0;
-int row_end=m;
-int column_start=0;
-int column_end=n;
+    cout<<"\n"<<endl;
+    while(row_start<row_end && column_start<column_end){
+        for(int i=row_start;i<row_end;i++){
+            cout<<"col:"<<i<<" row:"<<column_start<<" "<<A[column_start][i]<<" \n";
+        }
+        column_start++;
+    //int p;
+    //cin>>p;
 
-cout<<"\n"<<endl;
-while(row_start<row_end && column_start<column_end)
-{
+        for(int i=column_start;i<column_end;i++){
+            cout<<"col:"<<row_end-1<<" row:"<<i<<" "<<A[i][row_end-1]<<" \n";
+        }
+        row_end--;
+        if(column_end>column_start){
+            for(int i=row_end-1;i>=row_start;i--){
+                cout<<"col:"<<i<<" row:"<<column_end-1<<" "<<A[column_end-1][i]<<"\n ";
+            }
+            column_end--;
+        }
+        if(row_start<row_end){
+            for(int i=column_end-1;i>=column_start;i--){
+                cout<<"col:"<<row_start<<" row:"<<i<<" "<<A[i][row_start]<<"\n ";
+            }
 
-for(int i=row_start;i<row_end;i++)
-{
-cout<<"col:"<<i<<" row:"<<column_start<<" "<<A[column_start][i]<<" \n";
+            row_start++;
+        }
 
-}
-column_start++;
-//int p;
-//cin>>p;
-
-for(int i=column_start;i<column_end;i++)
-{
-cout<<"col:"<<row_end-1<<" row:"<<i<<" "<<A[i][row_end-1]<<" \n";
-
-}
-row_end--;
-if(column_end>column_start){
-for(int i=row_end-1;i>=row_start;i--)
-{
-cout<<"col:"<<i<<" row:"<<column_end-1<<" "<<A[column_end-1][i]<<"\n ";
-
-}
-column_end--;
-}
-if(row_start<row_end){
-for(int i=column_end-1;i>=column_start;i--)
-{
-cout<<"col:"<<row_start<<" row:"<<i<<" "<<A[i][row_start]<<"\n ";
-
-}
-
-row_start++;
-}
-
-}
+    }
 
 
 
-return ;
+    return ;
 }
 
 int main(){
